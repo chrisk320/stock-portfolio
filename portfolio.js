ca77c27ffccc2b5c6be1b1f0a3d081f515599a29
Christian Kim <chris.kim320@gmail.com>
2024-09-07 22:55:32 -0400

    setup and searchStock function

---
+++ portfolio.js
@@ -0,0 +1,54 @@
+let debounceTimer;
+
+function debounceSearch() {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(searchStock, 500);
+}
+
+
+async function searchStock() {
+    const query = document.getElementById('searchbar').value.trim();
+
+    if (!query) {
+        document.getElementById('dropdown').innerHTML = '';
+        return;
+    }
+
+    const apiKey = 'crecbahr01qnd5cv0n30crecbahr01qnd5cv0n3g';
+    const url = `https://finnhub.io/api/v1/search?q=${query}&token=${apiKey}`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        
+        if (data.result && data.result.length > 0) {
+            const usStocks = data.result.filter(symbol => {
+                return symbol.symbol.match(/^[A-Za-z]+$/);
+            });
+            if (usStocks.length > 0) {
+                displayDropdown(usStocks);
+            } else {
+                document.getElementById('dropdown').innerHTML = `<div class="no-results">No US stocks found</div>`                
+            }
+        } else {
+            document.getElementById('dropdown').innerHTML = `<div class="no-result">No results found</div>`;
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        document.getElementById('dropdown').innerHTML = `<div class="no-result">Error fetching data</>`;
+    }
+}
+
+function displayDropdown(symbols) {
+    const dropdown = document.getElementById('dropdown');
+    dropdown.innerHTML = '';
+    const tenSymbols = symbols.slice(0, 10);
+
+    tenSymbols.forEach(symbol => {
+        const symbolItem = document.createElement('div');
+        symbolItem.classList.add('dropdown-item');
+        symbolItem.innerHTML = `<strong>${symbol.symbol}</strong> (${symbol.description})`;
+        dropdown.appendChild(symbolItem);
+    });
+}
